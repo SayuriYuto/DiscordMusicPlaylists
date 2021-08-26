2ae9703a0c3d929762c3c9fcb7a7ed7dc58f6239
SayuriYuto <88235763+dhuriamey02@users.noreply.github.com>
2021-08-26 22:34:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ for fcogs in os.listdir("./cogs"):
   if fcogs.endswith(".py") and fcogs != "__init__.py":
     doki.load_extension(f'cogs.{fcogs[:-3]}')
 
+# Need to have the discord token in an env file. Refer various tutorials regarding Basic discord bot setup on Youtube
 doki.run(os.getenv("Token"))
